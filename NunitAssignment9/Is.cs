afec7ce5f5435eaa52c133035952fd46f94326d0
ArjunChandarana <achandarana801@gmail.com>
2021-05-25 12:51:00 +0530

    Final

---
+++ NunitAssignment9/Is.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitAssignment9
+{
+    public class Is : NUnit.Framework.Is
+    {
+        public static IsCapitalizeConstraint IsCapitalize(string expected)
+        {
+            return new IsCapitalizeConstraint(expected);
+        }
+
+        public static AlmostEqualToConstraint IsAlmostEqualTo(int expected, int percentageTolerance = 5)
+        {
+            return new AlmostEqualToConstraint(expected, percentageTolerance);
+        }
+
+        public static IsPartOfConstraint IsPartOf(int[] expected)
+        {
+            return new IsPartOfConstraint(expected);
+        }
+    }
+}
---
+++ NunitAssignment9/IsPartOfConstraint.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitAssignment9
+{
+    public class IsPartOfConstraint : Constraint
+    {
+        readonly int[] _expected;
+        public IsPartOfConstraint(int[] expected)
+        {
+            _expected = expected;
+            Description = $"Expected to be part of array.";
+        }
+        public override ConstraintResult ApplyTo<TActual>(TActual actual)
+        {
+            if (typeof(TActual) != typeof(int))
+                return new ConstraintResult(this, actual, ConstraintStatus.Error);
+
+            var actualInt = Convert.ToInt32(actual);
+
+            var result = false;
+            for (int i = 0; i < _expected.Length; i++)
+            {
+                if (_expected[i] == actualInt)
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            if (result == true)
+                return new ConstraintResult(this, actual, ConstraintStatus.Success);
+            else
+                return new ConstraintResult(this, actual, ConstraintStatus.Failure);
+        }
+    }
+}
